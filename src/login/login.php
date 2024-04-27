d60b51a55cbb0fffaca7b8437bfd02a391fb6278
y28lyn <i.beirade@gmail.com>
2024-04-27 16:52:26 +0200

    fix: :bug: fix

--- src/login/login.php
+++ src/login/login.php
@@ -2,7 +2,8 @@
 include '../../includes/connexion.php';
 
 // Fonction pour démarrer la session et rediriger l'utilisateur
-function startSessionAndRedirect($role, $participantId, $idResp = null) {
+function startSessionAndRedirect($role, $participantId, $idResp = null)
+{
     session_start();
     $_SESSION['role'] = $role;
     $_SESSION['participantId'] = $participantId;
@@ -51,6 +52,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -59,18 +61,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Connexion</title>
 </head>
+
 <body>
     <div class="flex justify-center h-screen bg-[#2B2E4A]">
         <!-- Panneau gauche -->
         <div class="hidden bg-cover lg:block lg:w-2/3" style="background-image: url(https://images.unsplash.com/photo-1465447142348-e9952c393450?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)">
-                <div class="flex items-center h-full px-20 bg-black bg-opacity-80">
-                    <div>
-                        <h2 class="text-4xl font-bold text-white">Renaissance urbaine</h2>
-                        
-                        <p class="max-w-xl mt-3 text-gray-300">Bâtisseurs de quartiers, l'association Renaissance Urbaine s'engage avec ferveur à revitaliser nos espaces citadins, créant ainsi des communautés florissantes où l'unité, la créativité et la solidarité sont les pierres angulaires de notre projet commun.</p>
-                    </div>
+            <div class="flex items-center h-full px-20 bg-black bg-opacity-80">
+                <div>
+                    <h2 class="text-4xl font-bold text-white">Renaissance urbaine</h2>
+
+                    <p class="max-w-xl mt-3 text-gray-300">Bâtisseurs de quartiers, l'association Renaissance Urbaine s'engage avec ferveur à revitaliser nos espaces citadins, créant ainsi des communautés florissantes où l'unité, la créativité et la solidarité sont les pierres angulaires de notre projet commun.</p>
                 </div>
-            </div>  
+            </div>
+        </div>
         <!-- Panneau droit -->
         <div class="flex items-center w-full max-w-md px-6 mx-auto lg:w-2/6">
             <div class="flex-1">
@@ -87,14 +90,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                         <div class="mt-6">
                             <div class="flex justify-between mb-2">
                                 <label for="password" class="text-sm text-gray-200">Mot de passe</label>
-                                <a href="#" class="text-sm text-gray-400 focus:text-[#FFF] hover:text-[#FFF] hover:underline">Mot de passe oublié?</a>
                             </div>
                             <input type="password" name="password" id="password" placeholder="Votre mot de passe" class="block w-full px-4 py-2 mt-2 bg-white border rounded-md placeholder-gray-600bg-gray-900 text-gray-900 border-gray-700 focus:border-[#FFF] focus:ring-[#FFF] focus:outline-none focus:ring focus:ring-opacity-40" />
                         </div>
                         <div class="mt-6">
-                            <button
-                                type="submit"
-                                class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-[#E84545] rounded-md hover:bg-[#903749] focus:outline-none focus:bg-[#903749] focus:ring focus:ring-[#FFF] focus:ring-opacity-50">
+                            <button type="submit" class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-[#E84545] rounded-md hover:bg-[#903749] focus:outline-none focus:bg-[#903749] focus:ring focus:ring-[#FFF] focus:ring-opacity-50">
                                 Se connecter
                             </button>
                         </div>
@@ -105,4 +105,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         </div>
     </div>
 </body>
+
 </html>
